8490053aec15b9bffc81bc512dc587fbbe14dc2d
hirotaka.iwasaki <nddgs@yahoo.co.jp>
2019-12-06 17:00:31 +0900

    201912061700

--- app/src/main/java/test/test191205n02_payjp_n03/MainActivity.kt
+++ app/src/main/java/test/test191205n02_payjp_n03/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
 import java.util.*
 import jp.pay.android.Payjp
 import jp.pay.android.PayjpConfiguration
@@ -55,5 +56,7 @@ class MainActivity : AppCompatActivity() {
             }
         })
         */
+
+        Snackbar.make(findViewById(android.R.id.content), "test191206n01", Snackbar.LENGTH_SHORT).show()
     }
 }
